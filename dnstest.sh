bd82374697e910650d07b994a98166f9a87b5df7
Tengfei X <tx.pro@outlook.com>
2018-11-23 11:46:57 +0800

    Modify providers and domain2test

--- dnstest.sh
+++ dnstest.sh
@@ -3,11 +3,11 @@
 command -v bc > /dev/null || { echo "bc was not found. Please install bc."; exit 1; }
 { command -v drill > /dev/null && dig=drill; } || { command -v dig > /dev/null && dig=dig; } || { echo "dig was not found. Please install dnsutils."; exit 1; }
 
-
-
 NAMESERVERS=`cat /etc/resolv.conf | grep ^nameserver | cut -d " " -f 2 | sed 's/\(.*\)/&#&/'`
 
 PROVIDERS="
+168.63.129.16#AzureInternal
+208.67.222.222#umbrella1 
 1.1.1.1#cloudflare 
 4.2.2.1#level3 
 8.8.8.8#google 
@@ -16,23 +16,20 @@ PROVIDERS="
 208.67.222.123#opendns 
 199.85.126.20#norton 
 185.228.168.168#cleanbrowsing 
-77.88.8.7#yandex 
-176.103.130.132#adguard 
-156.154.70.3#neustar 
-8.26.56.26#comodo
+216.146.35.35#dyn 
 "
 
-# Domains to test. Duplicated domains are ok
-DOMAINS2TEST="www.google.com amazon.com facebook.com www.youtube.com www.reddit.com  wikipedia.org twitter.com gmail.com www.google.com whatsapp.com"
+# Domains to test. Duplicated domains are ok. Top 10 China Websites.
+DOMAINS2TEST="baidu.com qq.com taobao.com tmall.com sohu.com jd.com sina.com.cn weibo.com 360.cn csdn.net alipay.com"
 
 
 totaldomains=0
 printf "%-18s" ""
 for d in $DOMAINS2TEST; do
     totaldomains=$((totaldomains + 1))
-    printf "%-8s" "test$totaldomains"
+    printf "%-12s" "$d"
 done
-printf "%-8s" "Average"
+printf "%-12s" "Average"
 echo ""
 
 
@@ -51,7 +48,7 @@ for p in $NAMESERVERS $PROVIDERS; do
 	        ttime=1
 	    fi
 
-        printf "%-8s" "$ttime ms"
+        printf "%-12s" "$ttime ms"
         ftime=$((ftime + ttime))
     done
     avg=`bc -lq <<< "scale=2; $ftime/$totaldomains"`
